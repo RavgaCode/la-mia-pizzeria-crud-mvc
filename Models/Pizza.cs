b3e09538699a893f5a9b238ff942e201c7e4d435
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-11-22 16:02:58 +0100

    Aggiungere i file di progetto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,39 @@
+﻿using la_mia_pizzeria.Validator;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(25)]
+        public string? Name { get; set; }
+
+
+        [Column(TypeName="text")]
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(100)]
+        [descriptionMinimumWords]
+        public string? Description { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(200)]
+        public string? Image { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public double? Price { get; set; }
+
+
+        public Pizza()
+        {
+
+        }
+        public Pizza (string name, string description, string image, double price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
